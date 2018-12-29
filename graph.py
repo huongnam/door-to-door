e24abb4db16f0e533ddb821dbdf97a570d155bcd
vnam <namvuhuong@gmail.com>
2018-12-29 18:35:13 +0700

    add argparse

--- graph.py
+++ graph.py
@@ -13,12 +13,68 @@ class Graph:
         return self.__nodes
 
 
-
-
     def distance(self, node1, node2):
         pos1 = node1.get_position()
         pos2 = node2.get_position()
         return ((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2) ** (1/2)
+
+
+    # def find_path(self):
+    #     temp = self.get_nodes()
+    #     best = temp
+    #     improved = True
+    #     while improved:
+    #         improved = False
+    #         for i in range(1, len(temp)-2):
+    #             for j in range(i+1, len(temp)):
+    #                 if j-i == 1: continue # changes nothing, skip then
+    #                 new_path = temp[:]
+    #                 new_path[i:j] = temp[j-1:i-1:-1] # this is the 2woptSwap
+    #                 total_distance = self.total_distance(best)
+    #                 new_total_distance = self.total_distance(new_path)
+    #                 if new_total_distance < total_distance:
+    #                     best = new_path
+    #                     improved = True
+    #         temp = best
+    #     return best
+
+    # def find_path(self):
+    #     nodes = self.get_nodes()
+    #     start = nodes[0]
+    #     path = [start]
+    #     nodes.remove(start)
+    #     while nodes:
+    #         next_node = min(nodes, key=lambda node: self.distance(path[-1], node))
+    #
+    #         path.append(next_node)
+    #         nodes.remove(next_node)
+    #     return path
+
+    def total_distance(self, path):
+        total_distance = 0
+        for i, node in enumerate(path[:-1]):
+            total_distance += self.distance(node, path[i + 1])
+        return total_distance
+
+
+    @staticmethod
+
+    def find_path(self):
+        return "haha"
+
+    def print_result(self):
+        path = self.find_path()
+        display = []
+        for node in path:
+            display.append(node.get_name())
+        print(display)
+        print("This covers {} khong phai km.".format(self.total_distance(path)))
+        print(len(path))
+
+
+class NearestNeighbor(Graph):
+    def __init__(self):
+        super().__init__()
 
 
     def find_path(self):
@@ -33,17 +89,27 @@ class Graph:
             nodes.remove(next_node)
         return path
 
-    def total_distance(self, path):
-        total_distance = 0
-        for i, node in enumerate(path[:-1]):
-            total_distance += self.distance(node, path[i + 1])
-        return total_distance
 
-    def print_result(self):
-        path = self.find_path()
-        display = []
-        for node in path:
-            display.append(node.get_name())
-        print(display)
-        print("This covers {} khong phai km.".format(self.total_distance(path)))
-        print(len(path))
+class TwoOpt(Graph):
+    def __init__(self):
+        super().__init__()
+
+
+    def find_path(self):
+        temp = self.get_nodes()
+        best = temp
+        improved = True
+        while improved:
+            improved = False
+            for i in range(1, len(temp)-2):
+                for j in range(i+1, len(temp)):
+                    if j-i == 1: continue # changes nothing, skip then
+                    new_path = temp[:]
+                    new_path[i:j] = temp[j-1:i-1:-1] # this is the 2woptSwap
+                    total_distance = self.total_distance(best)
+                    new_total_distance = self.total_distance(new_path)
+                    if new_total_distance < total_distance:
+                        best = new_path
+                        improved = True
+            temp = best
+        return best
--- n.py
+++ n.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
-import sys
+import argparse
 from node import Node
-from graph import Graph
+from graph import Graph, NearestNeighbor, TwoOpt
 from timeit import default_timer
+
+def process_argparse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--algo', help='specify which algorithm to use for\
+                        finding path among [nearest_neighbor|two_opt],\
+                        default nam', type=str, default='nam')
+    parser.add_argument('F', help='a file containing a list of cities to visit',
+                        type=str)
+    return parser.parse_args()
 
 
 def get_content(file):
@@ -14,25 +23,59 @@ def get_content(file):
         cities.append([line[0], float(line[1]), float(line[2])])
     return cities
 
-def set_graph(nodes):
-    graph = Graph()
-    graph.set_nodes(nodes)
-    graph.print_result()
+
+# def set_graph(nodes):
+#     graph = Graph()
+#     graph.set_nodes(nodes)
+#     graph.print_result()
 
 
 def main():
     start = default_timer()
-    cities = get_content(sys.argv[1])
+    args = process_argparse()
+    cities = get_content(args.F)
     nodes = [Node(city) for city in cities]
-    set_graph(nodes)
+    # graph = NearestNeighbor()
+    # print(type(graph))
+    # graph = []
+    graph = Graph()
+    if args.algo == "nam":
+        graph = NearestNeighbor()
+        graph.set_nodes(nodes)
+        new = graph.find_path()
+        graph = TwoOpt()
+        graph.set_nodes(new)
+    elif args.algo == "nearest_neighbor":
+        print("nearest_neighbor")
+        graph = NearestNeighbor()
+    elif args.algo == "two_opt":
+        print("two_opt")
+        graph = TwoOpt()
+
+    graph.set_nodes(nodes)
+    graph.print_result()
     end = default_timer()
     _time = end - start
     print("And it took {}s to find this!".format(_time))
     # print(cities)
 
-    # for city in cites:
+    # for city in cities:
     #     city = Node()
     # graph = Graph(cities)
+
+
+
+    # lst = args.N
+    # if args.gui and len(lst) > 15:
+    #     too_large()
+    #     exit()
+    # if check(lst) == 'Sorted':
+    #     if args.algo == 'merge':
+    #         merge_sort(lst)
+    #     if args.algo == 'quick':
+    #         quick_sort(lst, 0, len(lst) - 1)
+    #     else:
+    #         exit()
 
 
 if __name__ == '__main__':
